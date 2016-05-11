724a9a0d53fbd73c742290ccf4553b516001de09
Luke <Luke@Luke-PC>
2016-05-11 16:24:12 -0700

    routing fix?

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,16 +1,21 @@
 import { Component } from 'angular2/core';
 import { JSONP_PROVIDERS } from 'angular2/http';
+import { ROUTER_PROVIDERS, RouteConfig } from 'angular2/router';
 import 'rxjs/Rx';
 import { HistoryListComponent } from './history/history-list.component';
 import { LocationComponent } from './location/location.component';
 import { HistoryService } from './history/history.service';
 
-@Component ({
+@Component({
 	selector: 'wh-app',
 	templateUrl: 'app/app.component.html',
 	directives: [HistoryListComponent, LocationComponent],
-	providers: [HistoryService, JSONP_PROVIDERS]
+	providers: [HistoryService, JSONP_PROVIDERS, ROUTER_PROVIDERS]
 })
+
+@RouteConfig([
+	{ path: '/', name: 'Home', component: AppComponent, useAsDefault: true }
+])
 
 export class AppComponent {
 	pageTitle: string = 'Weather History';
