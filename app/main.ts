e29b79637c068abf1b7befab75d68bf3b0ac614c
Luke <Luke@Luke-PC>
2016-05-12 13:01:14 -0700

    enable prod mode

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,7 @@
 import { bootstrap } from 'angular2/platform/browser';
+import {enableProdMode} from 'angular2/core';
 import { AppComponent } from './app.component';
 import {ANGULAR2_GOOGLE_MAPS_PROVIDERS} from 'angular2-google-maps/core';
 
+enableProdMode();
 bootstrap(AppComponent, [ANGULAR2_GOOGLE_MAPS_PROVIDERS]);
