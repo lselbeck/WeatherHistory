b10f1d306e01d670499f012b67aa8ca8ff012c6c
Luke <Luke@Luke-PC>
2016-05-12 13:48:50 -0700

    more fixes

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,5 @@
 import { bootstrap } from 'angular2/platform/browser';
-import {enableProdMode} from 'angular2/core';
+import {enableProdMode, Directive} from 'angular2/core';
 import { AppComponent } from './app.component';
 import {ANGULAR2_GOOGLE_MAPS_PROVIDERS} from 'angular2-google-maps/core';
 
